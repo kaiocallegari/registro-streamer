dc912d18a962955e0cfce073b61b2bd5060c81e6
kaio <lorenaekaio@gmail.com>
2026-08-01 01:46:51 -0300

    mensagem 20

--- config.py
+++ config.py
@@ -24,26 +24,26 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 # ------------------------------------------------------------------
 # CARGOS (Roles) que definem o tier do criador
 # ------------------------------------------------------------------
-CARGO_TIER_4_ID = 1532529380046409728    # ID do cargo "Tier 4"
-CARGO_TIER_3_ID = 1532529390129381469    # ID do cargo "Tier 3"
-CARGO_TIER_2_ID = 1532529391563968542    # ID do cargo "Tier 2"
-CARGO_TIER_1_ID = 1532529393379840150    # ID do cargo "Tier 1"
+CARGO_TIER_4_ID = 1471198191046099140    # ID do cargo "Tier 4"
+CARGO_TIER_3_ID = 1471198192094937251    # ID do cargo "Tier 3"
+CARGO_TIER_2_ID = 1471198193248239637    # ID do cargo "Tier 2"
+CARGO_TIER_1_ID = 1471198194401542244    # ID do cargo "Tier 1"
 
 # Cargos que podem usar comandos administrativos (ex: /resetar_ranking, /enviar_painel)
 # Coloque quantos cargos quiser nessa lista — todos eles contam como admin.
 CARGO_ADMIN_IDS = [
-    1532494948501422090,  # Cargo admin 1
-    1532494949717643454,  # Cargo admin 2
-    1532494950116102295,  # Cargo admin 3
+    1465837667030925417,  # Cargo admin 1
+    1471194086403997851,  # Cargo admin 2
+    1465837667030925413,  # Cargo admin 3
 ]
 
 # ------------------------------------------------------------------
 # CANAIS DE LOG — um canal separado para cada tier
 # ------------------------------------------------------------------
-CANAL_LOG_TIER_4_ID = 1532560074613063680   # canal de log do Tier 4
-CANAL_LOG_TIER_3_ID = 1532559915284041910   # canal de log do Tier 3
-CANAL_LOG_TIER_2_ID = 1532560027460702268   # canal de log do Tier 2
-CANAL_LOG_TIER_1_ID = 1532560049850028112   # canal de log do Tier 1
+CANAL_LOG_TIER_4_ID = 1471278015584534754   # canal de log do Tier 4
+CANAL_LOG_TIER_3_ID = 1471278255880278180   # canal de log do Tier 3
+CANAL_LOG_TIER_2_ID = 1465837678002966704   # canal de log do Tier 2
+CANAL_LOG_TIER_1_ID = 1465837677470552125   # canal de log do Tier 1
 
 # ------------------------------------------------------------------
 # REGRAS DE META
